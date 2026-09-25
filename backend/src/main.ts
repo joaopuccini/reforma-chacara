058e871da0dc00a8ad75c4fe9cb288ff70c2d49e
Joao Pedro <joaoppuccini@gmail.com>
2026-09-25 10:01:01 -0300

    fix: disable default body parser to allow 50mb payload limit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
   const configService = app.get(ConfigService);
   
   app.enableCors();
